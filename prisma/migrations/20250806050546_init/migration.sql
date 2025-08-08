0f97329ce505c4822a23c4b560fa467189a4b581
sandro12352 <157259489+sandro12352@users.noreply.github.com>
2025-08-07 22:56:26 -0500

    cambios

--- prisma/migrations/20250806050546_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `completedAt` on the `todo` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
-
-*/
--- AlterTable
-ALTER TABLE `todo` MODIFY `completedAt` TIMESTAMP NULL;
--- prisma/migrations/20250808035437_init/migration.sql
+++ prisma/migrations/20250808035437_init/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `Todo` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `text` VARCHAR(50) NOT NULL,
-    `completedAt` TIMESTAMP NOT NULL,
+    `completedAt` TIMESTAMP NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
